fab208b268cbc2c02c661f9f75449fc68bdcac88
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-09-11 17:49:07 +0500

    Initial commit

---
+++ app/src/main/java/com/juraj/composespeedtest/UiState.kt
@@ -0,0 +1,9 @@
+package com.juraj.composespeedtest
+
+data class UiState(
+    val arcValue: Float = 0f,
+    val speed: String = "0.0 mbps",
+    val ping: String = "-",
+    val maxSpeed: String = "100",
+    val inProgress: Boolean = false
+)
